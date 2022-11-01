d411f531fc2594f79cd32f58dd1ea19f52a7a91a
Olivia Guzzardo <95261576+olguzzar@users.noreply.github.com>
2022-10-31 20:45:46 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,30 +6,4 @@ app.Urls.Add("http://localhost:3000");
 
 app.MapGet("/", () => "Hello All Things Open!");
 
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
-
 app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
-{
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
-}
-
-public record Weather
-{
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
-}
